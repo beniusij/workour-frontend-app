023a418a4f31aef3f7fd2e768d5c8425e63ede03
Juozas Joe Beniusis <beniusij@gmail.com>
2019-09-15 16:32:43 +0100

    Update register form to use validation with hooks

---
+++ src/components/form/RegisterForm.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import useForm from '../../hooks/UseForm.js'
+import Button from '../button/Button.js'
+import MandatoryInputField from '../field/MandatoryInputField.js'
+import styles from './Form.module.scss'
+
+function RegisterForm() {
+	const stateSchema = {
+		firstname: { value: '', error: '' },
+		lastname: { value: '', error: '' },
+		email: { value: '', error: '' },
+		password: { value: '', error: '' },
+		confirmpassword: { value: '', error: '' },
+	}
+
+	const validationStateSchema = {
+		firstname: {
+			required: true,
+			validator: {
+				regEx: /^[a-zA-Z]+$/,
+				error: 'Invalid first name format',
+			},
+		},
+		lastname: {
+			required: true,
+			validator: {
+				regEx: /^[a-zA-Z]+$/,
+				error: 'Invalid first name format',
+			},
+		},
+		email: {
+			required: true,
+			validator: {
+				regEx: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+				error: 'Invalid email address',
+			},
+		},
+		password: {
+			required: true,
+			validator: {
+				regEx: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+				error: 'Invalid password. It must be minimum of 8 chars, contain a number, upper case & lower case characters',
+			},
+		},
+		confirmpassword: {
+			required: true,
+			validator: {
+				regEx: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+				error: 'Invalid password. It must be minimum of 8 chars, contain a number, upper case & lower case characters',
+			},
+		},
+	}
+
+	const onSubmitForm = (state) => {
+		alert(JSON.stringify(state, null, 2))
+	}
+
+	const { state, handleOnChange, handleOnSubmit, disable } = useForm(
+		stateSchema,
+		validationStateSchema,
+		onSubmitForm
+	)
+
+	return (
+		<div className={styles.formContainer} >
+	    	<h2 className={styles.formTitle}>Sign Up</h2>
+	    	<form onSubmit={handleOnSubmit}>
+	    		<MandatoryInputField
+	    			name="First Name"
+	    			type="text"
+	    			function={handleOnChange} 
+	    		/>
+	    		<MandatoryInputField
+	    			name="Last Name"
+	    			type="text"
+	    			function={handleOnChange} 
+	    		/>
+		    	<MandatoryInputField 
+		    		name="Email" 
+		    		type="email" 
+		    		function={handleOnChange} 
+		    	/>
+		    	<MandatoryInputField 
+		    		name="Password" 
+		    		type="password"
+		    		function={handleOnChange} 
+		    	/>
+		    	<MandatoryInputField 
+		    		name="Confirm Password" 
+		    		type="password"
+		    		function={handleOnChange} 
+		    	/>
+		      <Button type={'submit'} text={'Sign In'} disabled={disable} />
+	    	</form>
+	    	
+		</div>
+	)
+}
+
+export default RegisterForm
---
+++ src/hooks/UseForm.js
@@ -0,0 +1,81 @@
+import { useState, useEffect, useCallback } from 'react';
+import variables from '../styles/variables.scss'
+
+function useForm(stateSchema, validationSchema = {}, callback) {
+	const [state, setState] = useState(stateSchema)
+	const [disable, setDisable] = useState(true)
+	const [isDirty, setIsDirty] = useState(false)
+
+	// Disabled button in initial render
+	useEffect(() => {
+		setDisable(true)
+	}, [])
+
+	// For every change in our state this will be fired
+	// To be able to disable the button 
+	useEffect(() => {
+		if (isDirty) {
+			setDisable(validateState());
+		}
+	}, [state, isDirty])
+
+	// Disable button if there is an error in state
+	const validateState = useCallback(() =>{
+		const hasErrorInState = Object.keys(validationSchema).some(key => {
+			const isInputFieldRequired = validationSchema[key].required
+			const stateValue = state[key].value
+			const stateError = state[key].error
+
+			return (isInputFieldRequired && !stateValue) || stateError
+		})
+	}, [state, validationSchema])
+
+	// Handle every change in every input
+	const handleOnChange = useCallback(event => {
+		setIsDirty(true)
+
+		const name = event.target.name
+		const value = event.target.value
+
+		let error = ''
+		if (validationSchema[name].required) {
+			if (!value) {
+				error = 'This is required field.'
+				event.target.style.borderColor = variables.error
+			} else {
+				if (
+					validationSchema[name].validator !== null &&
+					typeof validationSchema[name].validator === 'object'
+				) {
+					if (value && !validationSchema[name].validator.regEx.test(value)) {
+						error = validationSchema[name].validator.error
+						event.target.style.borderColor = variables.error
+					} else {
+						event.target.style.borderColor = variables.success
+					}
+				}
+			}
+		} else {
+			event.target.style.borderColor = variables.success
+		}
+
+		setState(prevState => ({
+			...prevState,
+			[name]: { value, error },
+		}))
+	}, [validationSchema])
+
+	const handleOnSubmit = useCallback(
+		event => {
+			event.preventDefault()
+
+			if (!validateState()) {
+				callback(state)
+			}
+		}, [state]
+	)
+
+	return { state, disable, handleOnChange, handleOnSubmit }
+}
+
+export default useForm
