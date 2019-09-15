50d8cebb6d9028426c88327e8dc346857ca358b3
Juozas Joe Beniusis <beniusij@gmail.com>
2019-09-15 16:33:20 +0100

    Updated Register page

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import RegisterForm from '../../components/form/RegisterForm.js'
+import styles from './Register.module.scss'
 
 const Register = props => {
 	return (
-		<div>
-			<p>This is registration page</p>
+		<div className={styles.page}>
+			<RegisterForm />
 		</div>
 	)
 }
