884e1b3e1f42e0f3285b6d1fafe9045d0dfc549f
Juozas Joe Beniusis <beniusij@gmail.com>
2019-10-19 16:15:58 +0100

    Modify component to use useMutation instead of component

--- src/components/form/RegisterForm.js
+++ src/components/form/RegisterForm.js
@@ -5,131 +5,135 @@ import MandatoryInputField from '../field/MandatoryInputField.js'
 import MandatoryCheckboxField from '../field/MandatoryCheckboxField.js'
 import styles from './Form.module.scss'
 import {gql} from "apollo-boost"
-import { Mutation } from 'react-apollo'
+import { useMutation } from '@apollo/react-hooks'
 
 function RegisterForm() {
-	const registerMutation = gql`
-		mutation RegisterMutation($first_name: String!, $last_name: String!, $email: String!, $password: String!, $password_confirm: String!) {
-			register(first_name: $first_name, last_name: $last_name, email: $email, password: $password, password_confirm: $password_confirm) {
-				id
-			}
-		}
-	`
+  const stateSchema = {
+    firstname: { value: '', error: '' },
+    lastname: { value: '', error: '' },
+    email: { value: '', error: '' },
+    password: { value: '', error: '' },
+    confirmpassword: { value: '', error: '' },
+    termsandconditions: { value: '', error: '' },
+  }
 
-	const stateSchema = {
-		firstname: { value: '', error: '' },
-		lastname: { value: '', error: '' },
-		email: { value: '', error: '' },
-		password: { value: '', error: '' },
-		confirmpassword: { value: '', error: '' },
-		termsandconditions: { value: '', error: '' },
-	}
+  const validationStateSchema = {
+    firstname: {
+      required: true,
+      validator: {
+        regEx: /^[a-zA-Z]+$/,
+        error: 'Invalid first name format.',
+      },
+    },
+    lastname: {
+      required: true,
+      validator: {
+        regEx: /^[a-zA-Z]+$/,
+        error: 'Invalid first name format.',
+      },
+    },
+    email: {
+      required: true,
+      validator: {
+        regEx: /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+.)+[^<>()[\].,;:\s@"]{2,})$/i,
+        error: 'Invalid email address.',
+      },
+    },
+    password: {
+      required: true,
+      validator: {
+        regEx: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+        error: 'Invalid password. It must be minimum of 8 chars, contain a number, upper case & lower case characters.',
+      },
+    },
+    confirmpassword: {
+      required: true,
+      validator: {
+        regEx: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+        error: 'Invalid password. It must be minimum of 8 chars, contain a number, upper case & lower case characters.',
+      },
+    },
+    termsandconditions: {
+      required: true,
+      validator: {
+        checked: true,
+        error: 'User must accept our terms and conditions to proceed with registration.'
+      }
+    }
+  }
 
-	const validationStateSchema = {
-		firstname: {
-			required: true,
-			validator: {
-				regEx: /^[a-zA-Z]+$/,
-				error: 'Invalid first name format.',
-			},
-		},
-		lastname: {
-			required: true,
-			validator: {
-				regEx: /^[a-zA-Z]+$/,
-				error: 'Invalid first name format.',
-			},
-		},
-		email: {
-			required: true,
-			validator: {
-				regEx: /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+.)+[^<>()[\].,;:\s@"]{2,})$/i,
-				error: 'Invalid email address.',
-			},
-		},
-		password: {
-			required: true,
-			validator: {
-				regEx: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
-				error: 'Invalid password. It must be minimum of 8 chars, contain a number, upper case & lower case characters.',
-			},
-		},
-		confirmpassword: {
-			required: true,
-			validator: {
-				regEx: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
-				error: 'Invalid password. It must be minimum of 8 chars, contain a number, upper case & lower case characters.',
-			},
-		},
-		termsandconditions: {
-			required: true,
-			validator: {
-				checked: true,
-				error: 'User must accept our terms and conditions to proceed with registration.'
-			}
-		}
-	}
+  const registerMutation = gql`
+    mutation RegisterMutation($first_name: String!, $last_name: String!, $email: String!, $password: String!, $password_confirm: String!) {
+      register(first_name: $first_name, last_name: $last_name, email: $email, password: $password, password_confirm: $password_confirm) {
+        ID
+      }
+    }
+  `
 
-	const onSubmitForm = event => {
-		return event.firstname, event.lastname, event.email, event.password, event.confirmpassword
-	}
+  const [register, { data }] = useMutation(registerMutation)
 
-	const { state, disable, handleOnChange, handleOnSubmit } = useForm(
-		stateSchema,
-		validationStateSchema,
-		onSubmitForm
-	)
+  const onSubmitForm = event => {
+    register({
+      variables: {
+        first_name: event.firstname.value,
+        last_name: event.lastname.value,
+        email: event.email.value,
+        password: event.password.value,
+        password_confirm: event.confirmpassword.value
+      }
+    })
+  }
 
-	return (
-		<div className={styles.formContainer} >
-	    	<h2 className={styles.formTitle}>Sign Up</h2>
-				<Mutation
-					mutation={registerMutation}
-					variables={{ }}
-				>
-					<form onSubmit={handleOnSubmit}>
-						<MandatoryInputField
-							name="First Name"
-							type="text"
-							function={handleOnChange}
-							error={state.firstname.error}
-						/>
-						<MandatoryInputField
-							name="Last Name"
-							type="text"
-							function={handleOnChange}
-							error={state.lastname.error}
-						/>
-						<MandatoryInputField
-							name="Email"
-							type="email"
-							function={handleOnChange}
-							error={state.email.error}
-						/>
-						<MandatoryInputField
-							name="Password"
-							type="password"
-							function={handleOnChange}
-							error={state.password.error}
-						/>
-						<MandatoryInputField
-							name="Confirm Password"
-							type="password"
-							function={handleOnChange}
-							error={state.confirmpassword.error}
-						/>
-						<MandatoryCheckboxField
-							name="Terms and Conditions"
-							type="checkbox"
-							function={handleOnChange}
-							error={state.termsandconditions.error}
-						/>
-							<Button type={'submit'} text={'Sign In'} disabled={disable} />
-					</form>
-				</Mutation>
-	    	
-		</div>
-	)
+  const { state, disable, handleOnChange, handleOnSubmit } = useForm(
+    stateSchema,
+    validationStateSchema,
+    onSubmitForm
+  )
+
+  return (
+    <div className={styles.formContainer} >
+      <h2 className={styles.formTitle}>Sign Up</h2>
+        <form onSubmit={handleOnSubmit}>
+          <MandatoryInputField
+            name="First Name"
+            type="text"
+            function={handleOnChange}
+            error={state.firstname.error}
+          />
+          <MandatoryInputField
+            name="Last Name"
+            type="text"
+            function={handleOnChange}
+            error={state.lastname.error}
+          />
+          <MandatoryInputField
+            name="Email"
+            type="email"
+            function={handleOnChange}
+            error={state.email.error}
+          />
+          <MandatoryInputField
+            name="Password"
+            type="password"
+            function={handleOnChange}
+            error={state.password.error}
+          />
+          <MandatoryInputField
+            name="Confirm Password"
+            type="password"
+            function={handleOnChange}
+            error={state.confirmpassword.error}
+          />
+          <MandatoryCheckboxField
+            name="Terms and Conditions"
+            type="checkbox"
+            function={handleOnChange}
+            error={state.termsandconditions.error}
+          />
+          <Button type={'submit'} text={'Sign In'} disabled={disable} />
+        </form>
+    </div>
+  )
 }
 
 export default RegisterForm
